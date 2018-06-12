6b1d8ac4b5476ed715f3d13c913a849fe561dd0d
sabbio93 <asabbioni@imolinfo.it>
2018-06-12 19:11:39 +0200

    implemented log in ampServer elk stack

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
+var apm = require('elastic-apm-node').start(
+  {
+    // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: 'OpenshiftProxy',
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: ' http://apmserver:8200',
+  });
 var express = require('express');
-var fs = require('fs');
 var app = express();
 var http = require('http');
-var path = require('path');
-var cfenv = require('cfenv');
-var dogapi = require("dogapi");
-
-var optionsDogAPi = {
- api_key: "2f5bcc61563c67c2a01b9eca54725b29",
- app_key: "f964330db51a858fe53a66cfff767ad3f116772d",
+var options = {
+  host: 'pdfconverterservice-openshiftproxy11.7e14.starter-us-west-2.openshiftapps.com'
 };
 
-dogapi.initialize(optionsDogAPi);
-// var DataDog = require('datadog');
-// var dogstatsd = new DataDog('2f5bcc61563c67c2a01b9eca54725b29', 'f964330db51a858fe53a66cfff767ad3f116772d');
-
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-//app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
 
 app.get('/', function (request, response) {
@@ -32,12 +20,15 @@ app.get('/', function (request, response) {
     response.end();
     });
 
+app.get('/error', function (request, response) {
+    var err = new Error('Ups, something broke!')
+    apm.captureError(err)
+    response.status(200);
+    response.send("y create an error");
+    response.end();
+    });
 
-var options = {
-  host: 'pdfconverterservice-openshiftproxy11.7e14.starter-us-west-2.openshiftapps.com'
-};
-
-callback = function(response,writer,tempi) {
+function callback(response,writer,tempi) {
     tempi.tempoServizio2 = Date.now();
       var str = '';
   //another chunk of data has been recieved, so append it to `str`
@@ -50,11 +41,6 @@ callback = function(response,writer,tempi) {
     writer.send(str);
     writer.end();
     tempi.tempoOverheadMid2=Date.now();
-    dogapi.metric.send("adapter_Openshift_overhead.time", tempi.tempoOverheadMid2-tempi.tempoOverheadMid1);
-    dogapi.metric.send("adapter_Service_Openshift_call.time", tempi.tempoServizio2-tempi.tempoServizio1);
-//     fs.appendFile(__dirname +'/log.txt', tempi+"\n", function (err) {
-//         console.log(err);
-//             });
   });
 }//callback
 
@@ -76,17 +62,6 @@ app.get('/pdftotext', function (request, response) {
     response.status(200);
     convert(response,tempi);
 });
-
-app.get('/filelog', function (request, response) {
-    response.sendFile(path.join(__dirname +'/log.txt'));
-    });
-
-app.get('/eliminafile', function (request, response) {
-    fs.unlink(__dirname +'/log.txt');
-    response.status(200);
-    response.send("eliminato");
-    response.end();
-    });
 
 app.get('/ping', function (request, response) {
     response.status(200);
