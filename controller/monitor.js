1f80f70c42f78ec5f710d6c1f428c59e468caf7f
sabbio93 <asabbioni@imolinfo.it>
2018-06-16 21:00:49 +0200

    problem with param error must switch to logStash

--- controller/monitor.js
+++ controller/monitor.js
@@ -1,9 +1,11 @@
 const si = require('systeminformation');
-var apm = require('elastic-apm-node');
+var apm = require('../bin/www');
+var log= apm.apm;
+var os = require('os');
 
 exports.send=function()
 {
-  /*system load
+    /*system load
   [ 0.84423828125, 0.5849609375, 0.44189453125 ]
   [1,5,15]
   */
@@ -11,8 +13,8 @@ exports.send=function()
     {
       let load=
       {
-        avg:os.loadavg(),
-        ncpu:os.cpus().length
+        avg: os.loadavg(),
+        ncpu: os.cpus().length
       }
       resolve(load);
     });
@@ -37,17 +39,38 @@ exports.send=function()
   var memLoad=si.mem();
 
   /*network load
-  {
-    iface: 'enp3s0',
-    operstate: 'up',
-    rx: 1940086186,
-    tx: 94073626,
-    rx_sec: 533.0305476752105,
-    tx_sec: 415.7018568548478,
-    ms: 221719
-  }
+  [ { iface: 'lxdbr0',
+      operstate: 'up',
+      rx: 559667,
+      tx: 5356664,
+      rx_sec: 8.356251322191664,
+      tx_sec: 39.39073408081235,
+      ms: 47270 },
+    { iface: 'veth2REFDP',
+      operstate: 'up',
+      rx: 927923,
+      tx: 3981716,
+      rx_sec: 104.92913052676116,
+      tx_sec: 94.43621747408504,
+      ms: 47270 },
+    { iface: 'vethQB8262',
+      operstate: 'up',
+      rx: 27618,
+      tx: 831427,
+      rx_sec: 0,
+      tx_sec: 26.656511805026657,
+      ms: 47268 } ]
   */
-  si.networkInterfaces().then((intf)=>si.networkStats(intf[1].iface));
-  si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').forEach(ifc=>si.networkStats(ifc.iface)));
+// var arrofP=  si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr))
+// var arrofP=  si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr)).then(ifaces=>ifaces.reduce((sum,iface)=>sum=sum+(iface.rx_sec/iface.ms)),0)
+  var netLoad=si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr).then(array=>Object.assign({},array.map(iface=>{return{"tx":iface.tx_sec,"rx":iface.rx_sec}}))))
+/*
+apm.captureError({
+  message: 'Could not find user %s with id %d in the database',
+  params: ['Peter', 42]
+})
+*/
+log.captureError({message:"ciao",params: [ram:1,cpu:2]});
+  //Promise.all([load,cpuLoad,memLoad,netLoad]).then(([load,cpuLoad,memLoad,netLoad])=>{return{load,cpuLoad,memLoad,netLoad}}).then(obj=>apm.captureError({}));
 
 }
