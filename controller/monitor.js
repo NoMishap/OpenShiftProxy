c2776c97688b139e552e54e5cfeafc411a9f5a5f
sabbio <sabbio.a93@gmail.com>
2018-06-18 01:55:06 +0200

    working metrics logging

--- controller/monitor.js
+++ controller/monitor.js
@@ -2,6 +2,19 @@ const si = require('systeminformation');
 var apm = require('../bin/www');
 var log= apm.apm;
 var os = require('os');
+var http = require('http');
+const querystring = require('querystring');
+
+const options = {
+  hostname: '88.147.126.145',
+  port: 8011 ,
+  path: '/',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
 
 exports.send=function()
 {
@@ -11,10 +24,14 @@ exports.send=function()
   */
   var load= new Promise(function(resolve,reject)
     {
+      let avg=os.loadavg();
+      let ncpu=os.cpus().length
       let load=
       {
-        avg: os.loadavg(),
-        ncpu: os.cpus().length
+        loadAvg1min: parseFloat(avg[0]),
+        loadAvg5min: parseFloat(avg[1]),
+        loadAvg15min: parseFloat(avg[2]),
+        ncpu,
       }
       resolve(load);
     });
@@ -25,7 +42,7 @@ exports.send=function()
     currentload: 4.540867810292633,
   }
   */
-  var cpuLoad =  si.currentLoad()
+  var cpuLoad =  si.currentLoad().then(cpu=>{return{'cpuLoad':cpu.currentload,'cpuAvgLoad':cpu.avgload}});
 
   /*memory load
   {
@@ -36,7 +53,14 @@ exports.send=function()
     available: 7790964736
   }
   */
-  var memLoad=si.mem();
+  var memLoad=si.mem().then(mem=>{
+    return {
+        totalMem: mem.total,
+        freeMem: mem.free,
+        usedMem: mem.used,
+        availableMem: mem.available
+      }
+  });
 
   /*network load
   [ { iface: 'lxdbr0',
@@ -45,32 +69,35 @@ exports.send=function()
       tx: 5356664,
       rx_sec: 8.356251322191664,
       tx_sec: 39.39073408081235,
-      ms: 47270 },
-    { iface: 'veth2REFDP',
-      operstate: 'up',
-      rx: 927923,
-      tx: 3981716,
-      rx_sec: 104.92913052676116,
-      tx_sec: 94.43621747408504,
-      ms: 47270 },
-    { iface: 'vethQB8262',
-      operstate: 'up',
-      rx: 27618,
-      tx: 831427,
-      rx_sec: 0,
-      tx_sec: 26.656511805026657,
-      ms: 47268 } ]
+      ms: 47270 },]
   */
 // var arrofP=  si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr))
 // var arrofP=  si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr)).then(ifaces=>ifaces.reduce((sum,iface)=>sum=sum+(iface.rx_sec/iface.ms)),0)
-  var netLoad=si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr).then(array=>Object.assign({},array.map(iface=>{return{"tx":iface.tx_sec,"rx":iface.rx_sec}}))))
-/*
-apm.captureError({
-  message: 'Could not find user %s with id %d in the database',
-  params: ['Peter', 42]
-})
-*/
-log.captureError({message:"ciao",params: [ram:1,cpu:2]});
-  //Promise.all([load,cpuLoad,memLoad,netLoad]).then(([load,cpuLoad,memLoad,netLoad])=>{return{load,cpuLoad,memLoad,netLoad}}).then(obj=>apm.captureError({}));
+//var netLoad=si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr).then(array=>Object.assign({},array.map(iface=>{return{"tx":parseFloat(iface.tx_sec),"rx":parseFloat(iface.rx_sec)}}))))
+  let netLoad=si.networkInterfaces().then(ifcs=>ifcs.filter((ifc)=>ifc.iface!=='lo').map(ifc=>ifc.iface).map(ifc=>si.networkStats(ifc))).then(arr=>Promise.all(arr));
+  let netLoadRx=netLoad.then(ar=>{return{netLoadRx:ar.map(v=>v.rx_sec).reduce((a,b)=>a+b)}});
+  let netLoadTx=netLoad.then(ar=>{return{netLoadTx:ar.map(v=>v.tx_sec).reduce((a,b)=>a+b)}});
+
+  Promise.all([load,cpuLoad,memLoad,netLoadRx,netLoadTx]).then(
+    ([load,cpuLoad,memLoad,netLoadRx,netLoadTx])=>
+      {
+        let metrics=Object.assign({},load,cpuLoad,memLoad,netLoadRx,netLoadTx);
+        //var metricsKV=JSON.stringify(metrics).replace(/:/g,"=").replace(/{|}/g,"").replace(/,/g," ");
+        let metricsKV=querystring.stringify(metrics," ");
+        let send= options;
+        Object.assign(send.headers,{'Content-Length': Buffer.byteLength(metricsKV)});
+        //console.log(metricsKV);
+
+        const req = http.request(options, (res) => {res.setEncoding('utf8')});
+
+        req.on('error', (e) => {
+          console.error(`problem with request: ${e.message}`);
+        });
+
+      // write data to request body
+        req.write(metricsKV);
+        req.end();
+
+      });
 
 }
